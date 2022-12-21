c0566871498ed6807abaac1ff7736ee1acb06971
Diyorbek-Sultanov <diyorbeksultonov1@mail.ru>
2022-12-21 10:42:16 +0500

    calculate and removing pizzas

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ window.addEventListener('DOMContentLoaded', () => {
 	const pizzas = document.getElementById('pizzas')
 	const cartList = document.getElementById('cart-list')
 	const cartCount = document.getElementById('cart-count')
+	const cartPrice = document.getElementById('price-value')
 
 	let pizzaId = 1
 
@@ -22,8 +23,23 @@ window.addEventListener('DOMContentLoaded', () => {
 		document.body.style.overflow = 'auto'
 	})
 
+	// Add pizza to cart
 	pizzas.addEventListener('click', purchasePizza)
 
+	// Removing pizzas
+	cartList.addEventListener('click', deletePizza)
+
+	// Update cart info
+	function updateInfoCart() {
+		let cartInfo = findToCart()
+
+		cartCount.textContent = cartInfo.count
+		cartPrice.textContent = cartInfo.total
+	}
+
+	updateInfoCart()
+
+	// Fetching pizzas from JSON File
 	function loadPizzas() {
 		fetch('./pizza.json')
 			.then(response => response.json())
@@ -50,6 +66,7 @@ window.addEventListener('DOMContentLoaded', () => {
 			})
 	}
 
+	// Pizza
 	function purchasePizza(e) {
 		if (e.target.classList.contains('btn--pizza')) {
 			let pizza = e.target.parentElement.parentElement
@@ -58,6 +75,7 @@ window.addEventListener('DOMContentLoaded', () => {
 		}
 	}
 
+	// Object pizza info
 	function pizzaInfo(pizza) {
 		let pizzaObj = {
 			id: pizzaId,
@@ -71,6 +89,7 @@ window.addEventListener('DOMContentLoaded', () => {
 		saveToPizzaInStorage(pizzaObj)
 	}
 
+	// Pizza to cart
 	function addToCart(pizza) {
 		const cartItem = document.createElement('div')
 		cartItem.setAttribute('data-id', `${pizza.id}`)
@@ -93,18 +112,23 @@ window.addEventListener('DOMContentLoaded', () => {
 		cartList.append(cartItem)
 	}
 
+	// Saving pizzas to LocalStorage
 	function saveToPizzaInStorage(item) {
 		let pizzas = getPizzaFromStorage()
 		pizzas.push(item)
 		localStorage.setItem('pizzas', JSON.stringify(pizzas))
+
+		updateInfoCart()
 	}
 
+	// Getting pizzas from LocalStorage
 	function getPizzaFromStorage() {
 		return localStorage.getItem('pizzas')
 			? JSON.parse(localStorage.getItem('pizzas'))
 			: []
 	}
 
+	// Cart load in refresh
 	function loadCart() {
 		let pizzas = getPizzaFromStorage()
 
@@ -115,5 +139,36 @@ window.addEventListener('DOMContentLoaded', () => {
 			pizzaId++
 		}
 		pizzas.forEach(pizza => addToCart(pizza))
+	}
+
+	// Calculate pizzas in cart
+	function findToCart() {
+		let products = getPizzaFromStorage()
+		let sum = products.reduce((total, product) => {
+			let price = parseFloat(product.price)
+
+			return (total += price)
+		}, 0)
+
+		return {
+			total: sum,
+			count: products.length,
+		}
+	}
+
+	// Delete pizza DOM and LocalStorage
+	function deletePizza(e) {
+		if (e.target.classList.contains('cart__item-close')) {
+			const pizza = e.target.parentElement
+			pizza.remove()
+
+			let products = getPizzaFromStorage()
+			let updateProduct = products.filter(product => {
+				return product.id !== parseInt(pizza.dataset.id)
+			})
+
+			localStorage.setItem('pizzas', JSON.stringify(updateProduct))
+			updateInfoCart()
+		}
 	}
 })
